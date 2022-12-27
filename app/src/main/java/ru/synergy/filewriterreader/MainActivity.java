fc110cc487138a506de29be95561be2fd2345c88
ankorgm <ankorgm@gmail.com>
2022-12-28 00:41:39 +0300

    Implemented Save text in main.activity

--- app/src/main/java/ru/synergy/filewriterreader/MainActivity.java
+++ app/src/main/java/ru/synergy/filewriterreader/MainActivity.java
@@ -1,8 +1,15 @@
 package ru.synergy.filewriterreader;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,11 +21,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    //Сохранение файла
+    public void saveText(View view) {
+        FileOutputStream fos = null;
+
+        try {
+            EditText textBox = (EditText) view.findViewById(R.id.editor);
+            String text = textBox.getText().toString();
+
+            fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+            fos.write(text.getBytes());
+            Toast.makeText(this, "Фаил успешно сохранен", Toast.LENGTH_LONG).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        } finally {
+            try {
+                if (fos != null) {
+                    fos.close();
+
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+
+            }
 
 
+        }
+
+    }
 
 
-
+             //Открытие файла
 
 
 }
